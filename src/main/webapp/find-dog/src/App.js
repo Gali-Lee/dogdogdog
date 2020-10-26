20501db7b204925649f3026ea5819a42d91e2ba8
gali-lee <taekyu32@gmail.com>
2020-10-26 16:20:03 +0900

    로그인페이지 추가

--- src/main/webapp/find-dog/src/App.js
+++ src/main/webapp/find-dog/src/App.js
@@ -5,6 +5,8 @@ import { Route } from 'react-router-dom';
 import Board1 from './pages/Board1/Board1';
 import Board2 from './pages/Board2/Board2';
 import Board3 from './pages/Board3/Board3';
+import Login from './pages/Login';
+import Map from './pages/map/Map'
 
 // 건들지 마시오!
 
@@ -12,9 +14,11 @@ function App() {
   return (
     <div>
       <Header />
+      <Route path="/login" exact={true} component={Login} />
       <Route path="/board1" exact={true} component={Board1} />
       <Route path="/board2" exact={true} component={Board2} />
       <Route path="/board3" exact={true} component={Board3} />
+      <Route path="/map" exact={true} component={Map} />
 
     </div>
   );
