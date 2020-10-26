d15c6dffc1c6c383cbe15a1ed7711219e87f1640
gali-lee <taekyu32@gmail.com>
2020-10-26 16:33:32 +0900

    ㅋㅋㅋ
    ㅋㅋㅋ

--- src/main/webapp/find-dog/src/App.js
+++ src/main/webapp/find-dog/src/App.js
@@ -5,7 +5,7 @@ import { Route } from 'react-router-dom';
 import Board1 from './pages/Board1/Board1';
 import Board2 from './pages/Board2/Board2';
 import Board3 from './pages/Board3/Board3';
-import Login from './pages/Login';
+import Login from './pages/login/Login';
 import Map from './pages/map/Map'
 
 // 건들지 마시오!
@@ -14,11 +14,7 @@ function App() {
   return (
     <div>
       <Header />
-<<<<<<< Updated upstream
       <Route path="/login" exact={true} component={Login} />
-=======
-      123123123
->>>>>>> Stashed changes
       <Route path="/board1" exact={true} component={Board1} />
       <Route path="/board2" exact={true} component={Board2} />
       <Route path="/board3" exact={true} component={Board3} />
