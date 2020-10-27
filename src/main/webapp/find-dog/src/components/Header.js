0b97cfe70f88476ff62fe2e9d3567a13dc68a6ee
gali-lee <taekyu32@gmail.com>
2020-10-27 16:42:41 +0900

    ㅁㅁ
    ㅁㅁ

--- src/main/webapp/find-dog/src/components/Header.js
+++ src/main/webapp/find-dog/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
 	return (
 		<div>
@@ -7,8 +8,8 @@ const Header = () => {
 			<Link to="/board1">게시판1</Link>
 			<Link to="/board2">게시판2</Link>
 			<Link to="/board3">게시판3</Link>
-
 		</div>
+
 	);
 };
 
