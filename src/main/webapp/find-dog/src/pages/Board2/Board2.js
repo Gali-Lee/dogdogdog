171a93b36f3968f8e214dd51b0f93b013917e807
kkami <kkami9090@google.com>
2020-10-29 16:23:13 +0900

    1.3.1 10_29 집보내기용 모달 작업중. ModalSetting과 ModalPage 추가되고 Modal을 분리해서 아이템화하는 작업필요.

--- src/main/webapp/find-dog/src/pages/Board2/Board2.js
+++ src/main/webapp/find-dog/src/pages/Board2/Board2.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import ListItem from './item/ListItem';
 import TestCard from './item/TestCard.js';
+import ModalPage from './ModalPage.js';
+
 
 
 
@@ -14,18 +15,16 @@ const Board2 = () => {
 		fetch("http://localhost:8000/board2/").then(res => res.json()).then(
 			res => {
 				setMeetings(res);
-				console.log(res);
 			}
 		);
 	}, [])
 
 	return (
 		<div>
+			
 			{/* 게시글 - 글번호,글제목,생성시간,장소,글쓴이
 			하단 - 조회,페이징. */}
-			<h1>모임 목록</h1>
-			<hr />
-
+			<h1>모임 목록</h1> <button onClick={<ModalPage/>}>aa</button>
 			{meetings.map(
 				meeting => {
 					return <TestCard key={meeting.mtId} meeting={meeting} />
