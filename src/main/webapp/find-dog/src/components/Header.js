e23654d3fd5130ca081ffeb5a4ccbe5c0fec4ffc
gali-lee <taekyu32@gmail.com>
2020-10-29 12:35:16 +0900

    1029오전

--- src/main/webapp/find-dog/src/components/Header.js
+++ src/main/webapp/find-dog/src/components/Header.js
@@ -19,10 +19,11 @@ const Header = () => {
 
 	return (
 		<div>
-			<Link to="/login">로그인페이지</Link>
-			<Link to="/board1">게시판1</Link>
-			<Link to="/board2">게시판2</Link>
-			<Link to="/board3">게시판3</Link>
+			<Link to="/login">로그인페이지</Link><br/>
+			<Link to="/board1">게시판1</Link><br/>
+			<Link to="/board2">게시판2</Link><br/>
+			<Link to="/board3">게시판3</Link><br/>
+			<Link to="/map">지도</Link>
 
 			{isLogin ?
 				(
