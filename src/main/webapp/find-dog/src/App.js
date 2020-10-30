b1d9608392ea5f2f818bd9851f5887029090829b
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-10-30 17:12:22 +0900

    1030 끝

--- src/main/webapp/find-dog/src/App.js
+++ src/main/webapp/find-dog/src/App.js
@@ -10,7 +10,11 @@ import Lside from './components/Lside';
 import Rside from './components/Rside';
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SiderBar from './components/SiderBar';
+import SubMenu from 'antd/lib/menu/SubMenu';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 /* 건들지 마시오!
 **************************
 ***************************
@@ -39,15 +43,58 @@ function App() {
     console.log(jwtToken);
   });
 
+const { Header1, Content, Sider } = Layout;
+
   return (
     <div>
-      <Header />
+  <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+  
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+
+            <Menu.Item key="1"  icon={<UserOutlined />}><Link to="/board1">게시판1</Link></Menu.Item>
+            <Menu.Item key="2" icon={<LaptopOutlined />}><Link to="/board2">게시판2</Link></Menu.Item>
+            <Menu.Item key="3" icon={<NotificationOutlined />} ><Link to="/board3">게시판3</Link></Menu.Item>
+            <Menu.Item key="4" icon={<NotificationOutlined />} ><Link to="/map">지도</Link></Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          <Main/>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>,
+    <Footer/>
+
+
+
+
+      {/* <Header />
       <MainListStyle>
         <Lside />
         <Main />
         <Rside />
       </MainListStyle>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
