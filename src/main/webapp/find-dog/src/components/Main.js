494c07f3deb3df445ff10bca9221f390c37c9c9f
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-11-06 13:32:44 +0900

    ㄴㅇㄹ
    ㄴㅇㄹ

--- src/main/webapp/find-dog/src/components/Main.js
+++ src/main/webapp/find-dog/src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter as Router,Route, Switch } from 'react-router-dom';
 import Login from '../pages/login/Login';
 import Board1 from '../pages/Board1/Board1';
 import Board2 from '../pages/Board2/Board2';
@@ -11,10 +11,14 @@ import Board4_1 from '../pages/Board4/Board4_1';
 import Board3Modify from '../pages/Board3/Board3Modify';
 import Board3Detail from '../pages/Board3/Board3Detail';
 import Board3Write from '../pages/Board3/Board3Write';
+
+
 const Main = () => {
 	return (
 		<div>
 			<Switch>
+
+
 				<Route path="/login" exact={true} component={Login} />
 				<Route path="/join" exact={true} component={Join} />
 				<Route path="/joindog" exact={true} component={DogJoin} />
